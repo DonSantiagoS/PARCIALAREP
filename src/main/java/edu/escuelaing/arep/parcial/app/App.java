d6b7938a921722c4a1ab22bd7870cfad24f788bf
Brayan Buitrago <brayan.buitrago@mail.escuelaing,edu.co>
2021-02-17 15:36:20 -0500

    Aplicacion

--- src/main/java/edu/escuelaing/arep/parcial/app/App.java
+++ src/main/java/edu/escuelaing/arep/parcial/app/App.java
@@ -1,5 +1,6 @@
 package edu.escuelaing.arep.parcial.app;
 
+import java.io.IOException;
 import spark.Request;
 import spark.Response;
 import static spark.Spark.*;
@@ -16,15 +17,62 @@ import static spark.Spark.*;
  * ---------------------------------------------------------------------------------------------------------------------------
  */
 public class App {
-	public static void main(String[] args) {
-		port(getPort());
-		get("/hello", (req, res) -> "Hello Heroku");
-	}
-	static int getPort() {
-		if (System.getenv("PORT") != null) {
-			return Integer.parseInt(System.getenv("PORT"));
-		}
-		return 4567; //returns default port if heroku-port isn't set
-		
-	}
+	/**
+	*	Main del spark quien realiza la consulta con las funciones Lampda
+	*	segun las vistas que se soliciten en la aplicacion
+	*	@param args
+    */
+    public static void main(String[] args) {
+        port(getPort());
+        get("/inputdata", (req, res) -> inputDataPage(req, res));
+        get("/clima", (req, res) -> resultsPage(req, res));
+    }
+
+	/**
+	*	@param req quien indica la informacion de la solicitud
+	*	@param res quien indica la informacion de respuesta
+	*	@return El clima en la ciudad solicitada en un json 
+	*/
+    private static String resultsPage(Request req, Response res) throws IOException {
+		HttpConnection web = new coneccion();
+		String solicitud = req.queryParams("lugar");
+		String json = http.getWeatherByCity(solicitud);
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "<style>"
+                + "body {text-align: center;"
+                + " font-family: \"new century schoolbook\";}"
+                + "h2 {text-align: center;}"
+                + "p {text-align: center;}"
+                + "a {text-align: center;}"
+                + "div {text-align: center;}"
+                + "form action {text-align: center;}"
+                + "</style>"
+                + "</head>"
+                +"<title>OpenWeather</title>"
+                + "<body style=\"background-color:rgb(249,255,201);\">"
+                + "<h2 text-aling =\"center\">El Json de la ciudad es </h2>"
+                + "<p>" +json+ "</p>"
+                + "<br>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    /**
+     * This method reads the default port as specified by the PORT variable in
+     * the environment.
+     *
+     * Heroku provides the port automatically so you need this to run the
+     * project on Heroku.
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
