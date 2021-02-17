1beca2ada087e025fbfa61637d891ea1deca2b9b
Brayan Buitrago <brayan.buitrago@mail.escuelaing,edu.co>
2021-02-17 14:41:22 -0500

    Parcial1

--- src/main/java/edu/escuelaing/arep/parcial/app/App.java
+++ src/main/java/edu/escuelaing/arep/parcial/app/App.java
@@ -16,8 +16,15 @@ import static spark.Spark.*;
  * ---------------------------------------------------------------------------------------------------------------------------
  */
 public class App {
-
-    public static void main(String[] args) {
- get("/hello", (req, res) -> "Hello Heroku");
- }
+	public static void main(String[] args) {
+		port(getPort());
+		get("/hello", (req, res) -> "Hello Heroku");
+	}
+	static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 4567; //returns default port if heroku-port isn't set
+		
+	}
 }
